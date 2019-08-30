779aa92d82f48f5373189475b6cb19abef858702
Lalit Garg <52823697+lalitgarg12@users.noreply.github.com>
2019-08-30 15:03:38 +0530

    Delete exercise1.py

--- exercise1.py
+++
@@ -1,5 +0,0 @@
-### Exercise - 01
-Write a program which repeatedly read numbers until the user enters done. Once "done" is entered, print out the total, count
-and average of the numbers. If the user enters anything other than numbers, detect their mistake by using try and except and print and error
-message and skip to the next number.
-
